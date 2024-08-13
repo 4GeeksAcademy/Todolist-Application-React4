16d8ca32fc42f17bad8f61eac59ce72bf7506c3e
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-08-13 20:06:47 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//Se identifica 2 estados 
+	const[inputValue,setInputValue]=useState("");
+	const[todos,setTodos]=useState(["estudiar", "lavar", "limpiar"]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center w-50">
+			<h1 className="text-center mt-5">todo</h1>
+			<ul class="list-group">
+				<li className="list-group-item">
+					<input type="text" className="form-control" placeholder="What needs to be done"
+					onChange={e => setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyDown={(e)=>{
+						if (e.key==="Enter"&& inputValue.trim() !== ""){
+							setTodos(todos.concat([inputValue]));
+							setInputValue("");
+						};
+					}}/>
+				</li>
+				{todos.map((t,index)=>(
+					<li key={index} className="list-group-item">{t}<i class="fas fa-trash-alt"></i></li>
+				))}
+			</ul>
+			<div>{todos.length}</div>
 		</div>
 	);
 };
